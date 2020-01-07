65d3a0444e5a763a7ea6db0723946d36fc90d399
ram kishor <ramkishor0509@gmail.com>
2020-01-07 21:19:19 +0530

    ok

---
+++ src/main/java/com/brijframwork/authorization/AuthorizationApplication.java
@@ -0,0 +1,18 @@
+package com.brijframwork.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableSwagger2
+public class AuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationApplication.class, args);
+	}
+
+}
